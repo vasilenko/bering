f98842c90a3883d32ee5e220196e8b69afbb6d4f
Nikolai Vasilenko <colormailbox@gmail.com>
2017-07-11 16:06:52 +0300

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import MainLayout from 'components/layouts/MainLayout';
 import BlogPage from 'components/containers/BlogPage';
+import Empty from 'components/containers/Empty';
 
 const App = () => (
-  <MainLayout>
-    <BlogPage />
-  </MainLayout>
+  <Router>
+    <MainLayout>
+      <Switch>
+        <Route exact path="/" component={BlogPage} />
+        <Route component={Empty}/>
+      </Switch>
+    </MainLayout>
+  </Router>
 );
 
 export default App;
---
+++ src/components/containers/Empty.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Header } from 'semantic-ui-react';
+
+const Empty = () => (
+  <div>
+    <Header>Page not found</Header>
+  </div>
+);
+
+
+export default Empty;
